82fc6eb027fab3792df69227c954b8ecfd74b834
Svetlana Krasavina <sgviktorova@gmail.com>
2026-08-13 09:42:14 +0300

    Применяем Pydantic в HTTP API клиентах. Практикуемся в применении Pydantic в API клиентах: документы

--- clients/http/gateway/documents/client.py
+++ clients/http/gateway/documents/client.py
@@ -1,31 +1,11 @@
-from typing import TypedDict
-
 from httpx import Response
 
 from clients.http.client import HTTPClient
 from clients.http.gateway.client import build_gateway_http_client
-
-
-class DocumentDict(TypedDict):
-    """
-    Описание структуры документа.
-    """
-    url: str
-    document: str
-
-
-class GetTariffDocumentResponseDict(TypedDict):
-    """
-    Описание структуры ответа на запрос получения тарифа по счету.
-    """
-    tariff: DocumentDict
-
-
-class GetContractDocumentResponseDict(TypedDict):
-    """
-    Описание структуры ответа на запрос получения контракта по счету.
-    """
-    contract: DocumentDict
+from clients.http.gateway.documents.schema import (
+    GetTariffDocumentResponseSchema,
+    GetContractDocumentResponseSchema
+)
 
 
 class DocumentsGatewayHTTPClient(HTTPClient):
@@ -51,25 +31,25 @@ class DocumentsGatewayHTTPClient(HTTPClient):
         """
         return self.get(f"/api/v1/documents/contract-document/{account_id}")
 
-    def get_tariff_document(self, account_id: str) -> GetTariffDocumentResponseDict:
+    def get_tariff_document(self, account_id: str) -> GetTariffDocumentResponseSchema:
         """
         Получение информации о тарифе по идентификатору счета.
 
         :param account_id: Идентификатор счета.
-        :return: Ответ от сервера (объект GetTariffDocumentResponseDict).
+        :return: Ответ от сервера (объект GetTariffDocumentResponseSchema).
         """
         response = self.get_tariff_document_api(account_id)
-        return response.json()
+        return GetTariffDocumentResponseSchema.model_validate_json(response.text)
 
-    def get_contract_document(self, account_id: str) -> GetContractDocumentResponseDict:
+    def get_contract_document(self, account_id: str) -> GetContractDocumentResponseSchema:
         """
         Получение информации о контракте по идентификатору счета.
 
         :param account_id: Идентификатор счета.
-        :return: Ответ от сервера (объект GetContractDocumentResponseDict).
+        :return: Ответ от сервера (объект GetContractDocumentResponseSchema).
         """
         response = self.get_contract_document_api(account_id)
-        return response.json()
+        return GetContractDocumentResponseSchema.model_validate_json(response.text)
 
 
 def build_documents_gateway_http_client() -> DocumentsGatewayHTTPClient:
