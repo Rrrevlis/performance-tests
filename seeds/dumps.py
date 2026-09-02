4e69dd1ee8ba25d0a7ae6c1de5ca238d1134acb4
Svetlana Krasavina <sgviktorova@gmail.com>
2026-09-02 10:54:01 +0300

    Добавляем логирование для нагрузочных тестов. Практика: работа с логированием в нагрузочных тестах

--- seeds/dumps.py
+++ seeds/dumps.py
@@ -1,6 +1,9 @@
 import os
 
 from seeds.schema.result import SeedsResult
+from tools.logger import get_logger
+
+logger = get_logger("SEEDS_DUMPS")
 
 
 def save_seeds_result(result: SeedsResult, scenario: str):
@@ -11,13 +14,16 @@ def save_seeds_result(result: SeedsResult, scenario: str):
     :param scenario: Название сценария нагрузки, для которого создаются данные.
                      Используется для генерации имени файла (например, "credit_card_test").
     """
+    seeds_file = f"./dumps/{scenario}_seeds.json"
     # Убедимся, что папка dumps существует
     if not os.path.exists("dumps"):
         os.mkdir("dumps")
 
     # Сохраняем результат сидинга в файл с именем {scenario}_seeds.json
-    with open(f"./dumps/{scenario}_seeds.json", 'w+', encoding="utf-8") as file:
+    with open(seeds_file, 'w+', encoding="utf-8") as file:
         file.write(result.model_dump_json())
+
+    logger.debug(f"Seeding result saved to file: {seeds_file}")
 
 
 def load_seeds_result(scenario: str) -> SeedsResult:
@@ -27,6 +33,9 @@ def load_seeds_result(scenario: str) -> SeedsResult:
     :param scenario: Название сценария нагрузки, данные которого нужно загрузить.
     :return: Объект SeedsResult, восстановленный из файла.
     """
+    seeds_file = f"./dumps/{scenario}_seeds.json"
     # Открываем файл и валидируем его как объект SeedsResult
-    with open(f'./dumps/{scenario}_seeds.json', 'r', encoding="utf-8") as file:
-        return SeedsResult.model_validate_json(file.read())
+    with open(seeds_file, 'r', encoding="utf-8") as file:
+        result = SeedsResult.model_validate_json(file.read())
+        logger.debug(f"Seeding result loaded from file: {seeds_file}")
+        return result
